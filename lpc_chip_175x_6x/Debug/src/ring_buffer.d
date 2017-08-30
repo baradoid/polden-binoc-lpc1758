3bd6f00b1ba1f3f2068b4f601397fa8d844870f3
barada <murinets.d@yandex.ru>
2017-08-30 17:56:29 +0300

    почищено

--- lpc_chip_175x_6x/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- C:\dev\polden-binoc\polden-binoc-lpc1758\lpc_chip_175x_6x\inc/ring_buffer.h \
- C:\dev\polden-binoc\polden-binoc-lpc1758\lpc_chip_175x_6x\inc/lpc_types.h
-
-C:\dev\polden-binoc\polden-binoc-lpc1758\lpc_chip_175x_6x\inc/ring_buffer.h:
-
-C:\dev\polden-binoc\polden-binoc-lpc1758\lpc_chip_175x_6x\inc/lpc_types.h:
